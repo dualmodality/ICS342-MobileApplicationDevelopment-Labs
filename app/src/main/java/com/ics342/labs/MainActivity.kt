8fb613581b396a17fe7a180107359bea699b305e
dualmodality <dualmodality@gmail.com>
2023-05-26 17:11:40 -0500

    JFear lab2 commit

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -7,14 +7,26 @@ import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var userText: EditText
+    private lateinit var button: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        userText = findViewById(R.id.user_input)
+        button = findViewById(R.id.my_button)
+        button.setOnClickListener {
+            handleButtonClick()
+        }
+
     }
 
     private fun handleButtonClick() {
-        /** Implement the functionality to display the alert here. **/
+        val userInput = userText.text.toString()
+        if (userInput.isEmpty()) {
+            showErrorAlert()
+        } else {
+            showTextInAlert(userInput)
+        }
     }
 
     private fun showTextInAlert(text: String) {
