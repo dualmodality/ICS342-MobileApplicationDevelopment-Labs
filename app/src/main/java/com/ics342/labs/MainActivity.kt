75cec0fec21ceb918a8c7b0cb673be3ea94a4506
dualmodality <dualmodality@gmail.com>
2023-06-04 20:10:08 -0500

    Lab 3 Commit

--- app/src/main/java/com/ics342/labs/MainActivity.kt
+++ app/src/main/java/com/ics342/labs/MainActivity.kt
@@ -3,13 +3,20 @@ package com.ics342.labs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ics342.labs.data.DataItem
 import com.ics342.labs.ui.theme.LabsTheme
 
@@ -52,26 +59,49 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+    /* Text(
         text = "Hello $name!",
         modifier = modifier
-    )
+    ) */
+    DataItemList(dataItems = dataItems)
 }
 
 @Composable
 fun DataItemView(dataItem: DataItem) {
-    /* Create the view for the data item her. */
+    Row {
+        Text(text = dataItem.id.toString(),
+                style = MaterialTheme.typography.headlineLarge)
+        Spacer(modifier = Modifier.size(8.dp))
+        Column {
+            Text(text = dataItem.name,
+                    style = MaterialTheme.typography.headlineSmall)
+            Spacer(modifier = Modifier.size(4.dp))
+            Text(text = dataItem.description,
+                    style = MaterialTheme.typography.bodySmall)
+            
+        }
+    }
+    
 }
 
 @Composable
 fun DataItemList(dataItems: List<DataItem>) {
     /* Create the list here. This function will call DataItemView() */
+    LazyColumn {
+        items(dataItems) {
+            DataItemView(dataItem = it)
+            Spacer(modifier = Modifier.size(8.dp))
+        }
+
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     LabsTheme {
-        Greeting("Android")
+       // Greeting("Android")
+        DataItemList(dataItems = dataItems)
     }
 }
