f740fa4e3d110d8a63ce4598a964b07593d580a5
Benjamin <benjamin.cassidy@metrostate.edu>
2023-07-31 18:10:55 -0500

    Lab 9

---
+++ app/src/test/java/com/ics342/labs/NumbersRepositoryTest.kt
@@ -0,0 +1,36 @@
+package com.ics342.labs
+
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+import java.util.UUID
+import kotlin.random.Random
+
+internal class NumbersRepositoryTest {
+
+    @Test
+    fun ifDatabaseDoesNotHaveANumberFetchItFromTheApi() {
+        val database = mockk<Database>()
+        val api = mockk<Api>()
+        val number = Number(UUID.randomUUID().toString(), Random.nextInt())
+        val id = number.id
+
+        every { database.getNumber(id) } returns null
+        every { api.getNumber(id) } returns number
+
+        val repository = NumbersRepository(database, api)
+        val result = repository.getNumber(id)
+
+        assertNotNull(result)
+
+        verify { database.getNumber(id) }
+        verify { api.getNumber(id) }
+    }
+
+    @Test
+    fun ifDatabaseIsEmptyShouldFetchNumbersFromApi() {
+        // TODO: implement this test
+    }
+}
